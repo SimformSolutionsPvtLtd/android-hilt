859f9ee686ed54b3d5d47bcddd1efac68db9b84c
patelpranay <patelpranay@simformsolutions.com>
2021-03-15 15:31:23 +0530

    [+] refactor LogFragment for all required dependency

--- app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -1,10 +1,15 @@
 package com.example.android.hilt.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.android.hilt.data.AppDatabase
+import com.example.android.hilt.data.LogDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 /**
  * Created by Simform Solutions on 15 March 2021.
@@ -14,7 +19,17 @@ import dagger.hilt.android.components.ApplicationComponent
 object DatabaseModule {
 
     @Provides
-    fun provideLogDao(appDatabase: AppDatabase) {
-        appDatabase.logDao()
+    fun provideLogDao(appDatabase: AppDatabase):LogDao {
+        return appDatabase.logDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "logging.db"
+        ).build()
     }
 }
